3cc6cf3e1881210188ab7f387f232fd0460697e0
Eduardo Souza de Oliveira <eduardosouza0301@gmail.com>
2025-05-26 20:06:57 -0300

    Publicando
    Segue Código Java Livro Bblioteca.java

---
+++ POOLivroBiblioteca.java
@@ -0,0 +1,18 @@
+public class POOLivroBiblioteca {
+
+    public static void main(String[] args) {
+        
+    LivroBiblioteca livro1 = new LivroBiblioteca();
+
+    livro1.setTitulo("Java: como programar");
+
+    livro1.devolver();
+
+    livro1.pegarEmprestado();
+
+    livro1.pegarEmprestado();
+
+    livro1.devolver(); 
+   
+    }
+}
